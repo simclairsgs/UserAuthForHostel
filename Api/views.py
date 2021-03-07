62fb02999a802b1a0410c55b2ee314b51738b0da
simclair.sgs <simclair.sgs@gmail.com>
2021-03-07 08:19:12 +0530

    login logic corrected + test conditions are added

--- Api/views.py
+++ Api/views.py
@@ -7,11 +7,10 @@ from rest_framework import status
 from .models import Master_Db,Login_Auth_Db
 from .serializers import Master_DbSerializer,Login_Auth_DbSerializer
 from backend_Mp import settings
+from . import tests
 
-
-# Create your views here.
 def index(request):
-    return HttpResponse('<h1>Hai i am sk</h1>')
+    return HttpResponse('<h1>You are Successfully connected to the Authentication Network.</h1>')
 
 
 @api_view(['POST'])
@@ -19,31 +18,26 @@ def login_user(request):
     register_no = request.data.get("Register_No")
     otp = request.data.get('Otp')
     key = request.data.get('Key')
-    print(register_no)
-    if(key == settings.SECRET_KEY):
+    if key == settings.SECRET_KEY or tests.TEST_MODE :
         try:
-
             register_num = Login_Auth_Db.objects.get(Register_No= register_no)
-            
+            if otp == register_num.Otp :
+                if tests.TEST_MODE:
+                    register_num.Otp = 123456
+                    register_num.save()
+                else:
+                    from random import randint
+                    create_random=(randint(10000000, 999999999))
+                    register_num.Otp = create_random
+                    register_num.save()
+            else:
+                return Response({"Error":"InvalidOtp"})    
         except:
-            return Response("Invalid userid")
-        try:
-            otpnum = Login_Auth_Db.objects.get(Otp = otp)
-        except:
-            return Response("Invalid Otp")
-        print("hai")
-        from random import randint
-        create_random=(randint(10000000, 999999999))
-        otpnum.Otp = create_random
-        otpnum.save()
-        
-        profile = Master_Db.objects.get(Register_No = register_no)
-        
-        serializer = Master_DbSerializer(profile,many=False)
-        
-        return Response(serializer.data)
-                
+            return Response({"Error":"InvalidUserId"})
 
+        profile = Master_Db.objects.get(Register_No = register_no)
+        serializer = Master_DbSerializer(profile,many=False)
+        return Response(serializer.data)
     else:
-        return Response("Invalid Key")
+        return Response({"Error":"InvalidKey"})
 
--- backend_Mp/settings.py
+++ backend_Mp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 't(j3zi6jwui$0r6+v94bbct!u^&^krwt-!qulz3(qm^7=mgpc1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
